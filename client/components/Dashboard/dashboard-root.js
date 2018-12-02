8ca85e224447dc5c0a62576ac9067e8a37017371
Jami Gibbs <jami0821@gmail.com>
2018-12-02 17:07:30 -0600

    Adding info card to display list count (#70)

--- client/components/Dashboard/dashboard-root.js
+++ client/components/Dashboard/dashboard-root.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import InfoBox from './info-box'
+import InfoCardListCount from './info-card-list-count'
+import { Grid } from '@material-ui/core'
 
 const DashboardRoot = () => {
   return (
@@ -7,6 +9,18 @@ const DashboardRoot = () => {
       <InfoBox
         title="Welcome to your dashboard"
         content="From here you can view information about your gift lists, create new lists, or update existing lists from the sidebar." />
+
+      <Grid container>
+        <Grid item xs={4}>
+          <InfoCardListCount />
+        </Grid>
+        <Grid item xs={4}>
+          <InfoCardListCount />
+        </Grid>
+        <Grid item xs={4}>
+          <InfoCardListCount />
+        </Grid>
+      </Grid>
     </div>
   )
 }
---
+++ client/components/Dashboard/info-card-list-count.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+const styles = theme => ({
+  card: {
+    marginTop: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit * 4,
+    marginTop: '25px',
+    maxWidth: '300px'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  pos: {
+    marginBottom: 12,
+  },
+})
+
+const InfoCardListCount = ({classes, lists}) => {
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          variant="h6"
+          align="center"
+          color="textSecondary" gutterBottom>
+          Total Lists Created
+        </Typography>
+        <Typography variant="h3" component="h3" align="center">
+          {lists.length}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+InfoCardListCount.propTypes = {
+  classes: PropTypes.object.isRequired,
+  lists: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    lists: state.list.userLists
+  }
+}
+
+export default connect(mapStateToProps, null)(withStyles(styles)(InfoCardListCount))
