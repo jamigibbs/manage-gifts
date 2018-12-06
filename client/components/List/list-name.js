cd255b06af9871e68052e3e1ae3f67d27f136353
Jami Gibbs <jami0821@gmail.com>
2018-12-06 07:14:07 -0600

    Adding editing toggle to list-item component

--- client/components/List/list-name.js
+++ client/components/List/list-name.js
@@ -1,17 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { TextField } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/edit'
 
-const ListName = ({userLists, listId}) => {
+class ListName extends React.Component {
 
-  const getListName = (id) => {
-    return userLists
+  state = {
+    editing: false,
+    text: ''
+  }
+
+  componentDidMount = () => {
+    const listId = this.props.listId
+    this.setState({text: this.getListName(listId)})
+  }
+
+  getListName = (id) => {
+    return this.props.userLists
       .find( list => list.id === id )
       .name
   }
 
-  return (
-    <span>{ getListName(listId) }</span>
-  )
+  handleClick = () => {
+    this.setState({editing: true})
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({editing: false})
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({text: event.target.value})
+  }
+
+  renderEdit = () => {
+    return <TextField
+      autoFocus
+      id="title-edit"
+      helperText="Press enter to save"
+      value={ this.state.text }
+      onChange={this.handleChange}
+      onKeyPress={this.handleKeyPress} />
+  }
+
+  renderDefault = () => {
+    return <div><span onClick={this.handleClick}>{ this.state.text }<EditIcon color="disabled" style={{ fontSize: 20, marginLeft: 10 }} /></span></div>
+  }
+
+  render(){
+    return (
+      <div>
+        { this.state.editing ? this.renderEdit() : this.renderDefault() }
+      </div>
+    )
+  }
 }
 
 ListName.propTypes = {
--- public/bundle.js
+++ public/bundle.js
@@ -2071,21 +2071,126 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/index.es.js");
+/* harmony import */ var _material_ui_icons_edit__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @material-ui/icons/edit */ "./node_modules/@material-ui/icons/edit.js");
+/* harmony import */ var _material_ui_icons_edit__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_edit__WEBPACK_IMPORTED_MODULE_3__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
-var ListName = function ListName(_ref) {
-  var userLists = _ref.userLists,
-      listId = _ref.listId;
 
-  var getListName = function getListName(id) {
-    return userLists.find(function (list) {
-      return list.id === id;
-    }).name;
-  };
 
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, getListName(listId));
-};
+
+var ListName =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(ListName, _React$Component);
+
+  function ListName() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, ListName);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(ListName)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
+      editing: false,
+      text: ''
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "componentDidMount", function () {
+      var listId = _this.props.listId;
+
+      _this.setState({
+        text: _this.getListName(listId)
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "getListName", function (id) {
+      return _this.props.userLists.find(function (list) {
+        return list.id === id;
+      }).name;
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleClick", function () {
+      _this.setState({
+        editing: true
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleKeyPress", function (e) {
+      if (e.key === 'Enter') {
+        _this.setState({
+          editing: false
+        });
+      }
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleChange", function (event) {
+      _this.setState({
+        text: event.target.value
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "renderEdit", function () {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_material_ui_core__WEBPACK_IMPORTED_MODULE_2__["TextField"], {
+        autoFocus: true,
+        id: "title-edit",
+        helperText: "Press enter to save",
+        value: _this.state.text,
+        onChange: _this.handleChange,
+        onKeyPress: _this.handleKeyPress
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "renderDefault", function () {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        onClick: _this.handleClick
+      }, _this.state.text, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_material_ui_icons_edit__WEBPACK_IMPORTED_MODULE_3___default.a, {
+        color: "disabled",
+        style: {
+          fontSize: 20,
+          marginLeft: 10
+        }
+      })));
+    });
+
+    return _this;
+  }
+
+  _createClass(ListName, [{
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, this.state.editing ? this.renderEdit() : this.renderDefault());
+    }
+  }]);
+
+  return ListName;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
 ListName.propTypes = {
   userLists: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.arrayOf(prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.object),
@@ -50511,6 +50616,38 @@ var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.d
 }), _react.default.createElement("path", {
   d: "M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z"
 })), 'PlaylistAdd');
+
+exports.default = _default;
+
+/***/ }),
+
+/***/ "./node_modules/@material-ui/icons/edit.js":
+/*!*************************************************!*\
+  !*** ./node_modules/@material-ui/icons/edit.js ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@material-ui/icons/node_modules/@babel/runtime/helpers/interopRequireDefault.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var _createSvgIcon = _interopRequireDefault(__webpack_require__(/*! ./utils/createSvgIcon */ "./node_modules/@material-ui/icons/utils/createSvgIcon.js"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("path", {
+  d: "M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+}), _react.default.createElement("path", {
+  fill: "none",
+  d: "M0 0h24v24H0z"
+})), 'Edit');
 
 exports.default = _default;
 
