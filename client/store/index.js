a8ad8477ef8a46544e7331c6c86662d4264e40a1
Jami Gibbs <jami0821@gmail.com>
2018-12-10 12:15:47 -0600

    Redux log only in dev environment (#91)

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,19 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { user, receivers, list, loading } from '../reducers'
 
 const reducer = combineReducers({ user, receivers, list, loading })
+const middlewares = [thunkMiddleware]
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger({collapsed: true})
+  middlewares.push(logger)
+}
 
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(...middlewares)
 )
 const store = createStore(reducer, middleware)
 
