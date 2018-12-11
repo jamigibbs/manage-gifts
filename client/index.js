0e845ddacac3901fd9fa06f421ed75a39d96a9b2
Jami Gibbs <jami0821@gmail.com>
2018-12-11 07:06:08 -0600

    Adding google analytics page view HOC (#96)
    * Adding google analytics page view HOC
    
    * Adding react-ga to package.json

--- client/index.js
+++ client/index.js
@@ -3,10 +3,11 @@ import '../styles/main.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
+import {Router, Route} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import withTracker from './with-tracker'
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -70,7 +71,8 @@ ReactDOM.render(
     <Router history={history}>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Route component={withTracker(App, { /* additional attributes */ } )} />
+        {/* <App /> */}
       </MuiThemeProvider>
     </Router>
   </Provider>,
