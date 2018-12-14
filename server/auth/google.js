2f7ca92d4fd8835662cc9be5a49c038c53190021
Jami Gibbs <jami0821@gmail.com>
2018-12-14 13:14:32 -0600

    Correcting casing of module name

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,4 +1,4 @@
-const Op = require('Sequelize').Op
+const Op = require('sequelize').Op
 const passport = require('passport')
 const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
